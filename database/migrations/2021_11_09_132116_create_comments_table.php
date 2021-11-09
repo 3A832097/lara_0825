2b029e188a585c290fee24a5d0f022dbde21301e
Hsin-Ju Teng <dlulu900821@gmail.com>
2021-11-09 21:24:58 +0800

    comments create&add content、post_id(前面commit沒有新增comments欄位)

--- database/migrations/2021_11_09_132116_create_comments_table.php
+++ database/migrations/2021_11_09_132116_create_comments_table.php
@@ -16,7 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->text('content');
-            $table->bigInteger('post_id');
+            $table->bigInteger("post_id");
             $table->timestamps();
         });
     }
